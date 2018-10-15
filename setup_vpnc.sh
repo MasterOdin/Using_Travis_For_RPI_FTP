43b200e62aa4e6bd8aa7e5b38b6851fe1d8b7dc5
Matthew Peveler <matt.peveler@gmail.com>
2018-10-15 18:56:27 -0400

    ...

--- setup_vpnc.sh
+++ setup_vpnc.sh
@@ -8,4 +8,4 @@ IPSec secret ${RPI_SECRET}
 # Only include the following lines if you want to store your username.and password
 # If they are not included, you will need to type in this information every time you connect to the VPN
 Xauth username ${RPI_USERNAME}
-Xauth password ${RPI_PASSWORD}" > /etc/vpnc/rpi.conf
+Xauth password ${RPI_PASSWORD}" > vpnc/default.conf
