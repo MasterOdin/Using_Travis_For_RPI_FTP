76c1e54d29d07b1742a9bc45e25875b513e83a2a
Matthew Peveler <matt.peveler@gmail.com>
2018-10-15 18:32:40 -0400

    initial commit

---
+++ setup_vpnc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo -e "
+IPSec gateway vpn.net.rpi.edu
+IPSec id rpi
+IPSec secret ${RPI_SECRET}
+
+# Only include the following lines if you want to store your username.and password
+# If they are not included, you will need to type in this information every time you connect to the VPN
+Xauth username ${RPI_USERNAME}
+Xauth password ${RPI_PASSWORD}" > /etc/vpnc/rpi.conf
